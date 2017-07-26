d25cf5b5a18d9caa9bc7df9040b2f82037784dc6
Łukasz Sypniewski <l.sypniewski@gmail.com>
2017-07-26 23:00:17 +0200

    Init commit - Unit tests for ConvertHTMDrawsToXML class

---
+++ NUnitTestProject1/Test_ConvertHTMLDrawsToXML.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using Lotto;
+
+namespace Lotto
+{
+    [TestFixture]
+    public class Test_ConvertHTMLDrawsToXML
+    {
+        [TestCase(10349, 3, 34)]
+        [TestCase(10339, 6, 16)]
+        [TestCase(5097, 1, 35)]
+        [TestCase(1977, 4, null)]
+        public void DivideTest(int DrawNumber, int number1, byte? plus)
+        {
+            List<Losowanie> listAllDraws = ConvertHTMLDrawsToXML.GetDrawsList(null, true, @"C:\Users\Sypcio\Documents\Dropbox\Docs\Programming\!Visual Studio 2015\Projects\Lotto_HTML_to_XML\Lotto_HTML_to_XML\TestReference.html", false);
+            Losowanie draw = listAllDraws.Select(d => d).Where(d => d.NrLosowania == DrawNumber).First();
+
+            Assert.AreEqual(DrawNumber, draw.NrLosowania);
+            Assert.AreEqual(number1, draw.Liczby[0]);
+            Assert.AreEqual(plus, draw.Plus);
+        }
+    }
+}
+
+
